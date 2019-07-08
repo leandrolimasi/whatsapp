16b7f8082855166758b47e0581a32d44cc81fc1a
leandrolimasi <leandrolimadasilva@gmail.com>
2019-07-08 20:25:40 -0300

    setup redux

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import Routes from './src/Routes';
+import reducers from './src/reducers'
 
 export default props => (
-  <Routes />
+  <Provider store={createStore(reducers)}>
+    <Routes />
+  </Provider>
+
 );
