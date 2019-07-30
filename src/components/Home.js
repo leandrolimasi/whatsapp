3ca5fcba0e9ff3cc69a93ab7a85923a104719b9b
leandrolimasi <leandrolimadasilva@gmail.com>
2019-07-30 08:54:21 -0300

    add tab view component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import { View, Text, Image, ImageBackground } from 'react-native';
-import { Button } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { Actions } from 'react-native-router-flux';
+import React, {Component} from 'react';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import { TabView, SceneMap } from 'react-native-tab-view';
 
-export default props => {
-    return (
-        <ImageBackground style={{ flex: 1, width: null }} source={require('../img/bg.png')}>
-            <View>
-                <Text>Home!</Text>
-            </View>
-        </ImageBackground>
-    )
+const ChatRoute = () => (
+    <View style={[styles.scene, { backgroundColor: '#ff4081' }]} />
+);
+
+const ContactRoute = () => (
+    <View style={[styles.scene, { backgroundColor: '#673ab7' }]} />
+);
+
+export default class Home extends Component {
+    state = {
+        index: 0,
+        routes: [
+            { key: 'first', title: 'Chats' },
+            { key: 'second', title: 'Contacts' },
+        ],
+    };
+
+    render() {
+        return (
+            <TabView
+                navigationState={this.state}
+                renderScene={SceneMap({
+                    first: ChatRoute,
+                    second: ContactRoute,
+                })}
+                onIndexChange={index => this.setState({ index })}
+                initialLayout={{ width: Dimensions.get('window').width }}
+            />
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    scene: {
+        flex: 1,
+    },
+});
