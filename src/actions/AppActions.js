15dd87eec681733d12c2bb558429011160ab78a5
leandrolimasi <leandrolimadasilva@gmail.com>
2019-09-19 08:42:08 -0300

    save user messages

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -1,7 +1,7 @@
 import firebase from "firebase";
 import base64 from "base-64";
 import _ from 'lodash';
-import { MODIFY_ADD_CONTACT_EMAIL, ADD_CONTACT, ADD_CONTACT_ERROR, ADD_CONTACT_SUCCESS, LIST_CONTACT_USER, MODIFY_MESSAGE } from '../constants/TypeConstants';
+import { MODIFY_ADD_CONTACT_EMAIL, ADD_CONTACT, ADD_CONTACT_ERROR, ADD_CONTACT_SUCCESS, LIST_CONTACT_USER, MODIFY_MESSAGE, SEND_MESSAGE } from '../constants/TypeConstants';
 
 export const modifyAddContactEmail = (text) => {
     return {
@@ -69,8 +69,32 @@ export const sendMessage = (message, contactName, contactEmail) => {
         const userEmailB64 = base64.encode(userEmail);
         const contactEmailB64 = base64.encode(contactEmail);
 
-        firebase.database().ref(`/message/${userEmailB64}/${contactEmailB64}`).push({ message, type: 'e' }).then(() => {
-            firebase.database().ref(`/message/${contactEmailB64}/${userEmailB64}`).push({ message, type: 'r' }).then(() => dispatch({ type: SEND_MESSAGE, payload: message }));
-        })
+        firebase.database()
+            .ref(`/message/${userEmailB64}/${contactEmailB64}`)
+            .push({ message, type: 'e' })
+            .then(() => {
+                firebase.database()
+                    .ref(`/message/${contactEmailB64}/${userEmailB64}`)
+                    .push({ message, type: 'r' })
+                    .then(() => dispatch({ type: SEND_MESSAGE, payload: message }));
+            })
+            .then(() => {
+                firebase.database().ref(`/user_messages/${userEmailB64}/${contactEmailB64}`).set({ name: contactName, email: contactEmail });
+            })
+            .then(() => {
+
+                firebase.database()
+                    .ref(`/contact/${userEmailB64}`)
+                    .once("value")
+                    .then(snapshot => {
+
+                        const userData = _.first(_.values(snapshot.val()));
+
+                        firebase.database()
+                            .ref(`user_messages/${contactEmailB64}/${userEmailB64}`)
+                            .set({ name: userData.name, email: userEmail });
+
+                    }); 
+            });
     }
 }
